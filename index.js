bf8d1984ca31f54cbe53e781d352ef0305c7ee73
lfrodriguez <lfrodriguez06@gmail.com>
2020-03-22 19:34:08 -0600

    auth routes implemented, installed passport, poassport-http, bcrypt, jsonwebtoken

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 
 const { config } = require('./config/index');
 
+const authApi = require('./routes/auth.js');
 const moviesApi = require('./routes/movies.js');
 const userMoviesApi = require('./routes/userMovies.js');
 
 app.use(express.json());
 
+authApi(app);
 moviesApi(app);
 userMoviesApi(app);
 
